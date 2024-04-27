2c39ab0bcf81263331e3625eacb7078db99c60b4
ThomasVrd11 <Thomasviard@orange.fr>
2024-04-27 18:14:52 +0200

    Fix variable declaration in AnimationAndMovementController.cs

--- Sandbox_Portfolio/Assets/Scripts/AnimationAndMovementController.cs
+++ Sandbox_Portfolio/Assets/Scripts/AnimationAndMovementController.cs
@@ -7,7 +7,7 @@ using UnityEngine.InputSystem;
 public class AnimationAndMovementController : MonoBehaviour
 {
     // * ########## Variables ########## * //
-    PlayerInput playerInput,
+    PlayerInput playerInput;
     CharacterController characterController;
     Animator animator;
 
