1088c9206597256eae1899410ab0f120468d3185
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-05-17 17:15:51 +0200

    Finished terrain / Grass shader / Loading screen

--- Sandbox_Portfolio/Assets/Scripts/PortalSceneSwap.cs
+++ Sandbox_Portfolio/Assets/Scripts/PortalSceneSwap.cs
@@ -2,16 +2,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class SceneSwapPortal : MonoBehaviour
 {
     public string sceneName;
-
+    GameObject loadingScreen;
+    
+    private void Start() {
+        loadingScreen = GameObject.Find("UI").transform.Find("Loading").gameObject;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(sceneName);
+            SceneManager.LoadSceneAsync(sceneName);
+            if (loadingScreen != null)
+            {
+            loadingScreen.SetActive(true);
+            }
         }
     }
 }
